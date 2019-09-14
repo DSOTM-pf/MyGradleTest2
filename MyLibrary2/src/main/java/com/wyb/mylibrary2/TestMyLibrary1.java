45977a43441ce4689455a7b3366088895d3bf245
wyb <1792195840@qq.vom>
2019-09-14 14:16:50 +0800

    依赖MyLibrary1,并使用

--- MyLibrary2/src/main/java/com/wyb/mylibrary2/TestMyLibrary1.java
+++ MyLibrary2/src/main/java/com/wyb/mylibrary2/TestMyLibrary1.java
@@ -1,12 +1,26 @@
 package com.wyb.mylibrary2;
 
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.example.mylibrary.People;
 
 //引用MyLibrary1，判断对依赖包中的依赖包的删除情况
 public class TestMyLibrary1 {
-    public static void Demo1()
+    public static void Demo1(Activity MainActivity)
     {
         //先不使用
-        //People people = new People();
+        People people = new People();
+        people.setName("wyb");
+        if(people.Eat(MainActivity))//如果方法成功运行，Toast提示
+        {
+            Toast toast = Toast.makeText(MainActivity,"MyGradle2",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+    public void Demo2()
+    {
+        Log.e("MyGradle:","Demo2");
     }
 }
