5f15eae1110d161e689e7653f04a69f220f5e03e
wyb <1792195840@qq.vom>
2019-09-14 13:13:06 +0800

    依赖MyLibrary1

---
+++ MyLibrary2/src/main/java/com/wyb/mylibrary2/TestMyLibrary1.java
@@ -0,0 +1,12 @@
+package com.wyb.mylibrary2;
+
+import com.example.mylibrary.People;
+
+//引用MyLibrary1，判断对依赖包中的依赖包的删除情况
+public class TestMyLibrary1 {
+    public static void Demo1()
+    {
+        //先不使用
+        //People people = new People();
+    }
+}
